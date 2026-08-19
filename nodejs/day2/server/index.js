2a4d2f65bcd213cf5574a52d9467e6035cc4f54b
Yash Malik <YOUR_GITHUB_EMAIL>
2026-08-20 00:09:48 +0530

    more changes

--- nodejs/day2/server/index.js
+++ nodejs/day2/server/index.js
@@ -2,9 +2,13 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req,res)=>{
-  const log = `${Date.now()}: New req recieved\n`;
+  const log = `${Date.now()}: ${req.url} New req recieved\n`;
   fs.appendFile('log.txt',log, (err,data)=>{
-    res.end("hello from my server");
+    switch(req.url){
+      case '/': res.end('Home Page'); break;
+      case '/about': res.end('About Page'); break;
+      default: res.end('404 Not Found'); break;
+    }
   })
 });
 
